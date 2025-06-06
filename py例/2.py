840bfe94652a354e8af6999b4178744a575f7053
lms <lms3082@outlook.com>
2025-06-06 15:06:46 +0800

    second learning

--- py例/2.py
+++ py例/2.py
@@ -1,1 +1,10 @@
-print("first learning python")
+x='hello'
+print(x,'lms',"\nLet's hhh")
+print('"Life is short,let\'s learn Python"')
+print(r"D:\home")
+print("D:\\home")
+print('''   
+    *
+  *   *
+''')
+print(('520'+'1314\n')*2)
